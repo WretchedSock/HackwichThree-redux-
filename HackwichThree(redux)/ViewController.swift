4b8db7a7eb398da3d7625387104c8e40dd328352
CM Student <cmstudent@UHWO-ACM-16.local>
2018-02-05 10:51:04 -1000

    Changed firstLabel text in the code, added ios11 glyph icons, changed Item 1 to About, and completed Hackwich Three

--- HackwichThree(redux)/ViewController.swift
+++ HackwichThree(redux)/ViewController.swift
@@ -6,10 +6,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var firstLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    //set firstLabel to the string "About"
+    
+        
+      self.firstLabel.text = "About"
+
     }
 
     override func didReceiveMemoryWarning() {
