7a902d1d89e24f112186829f0a910eef745ac26a
CM Student <cmstudent@UHWO-ACM-16.local>
2018-03-12 12:28:39 -1000

    Not done yet. Finished the first tab, moving on to the second tab

--- HackwichThree(redux)/ViewController.swift
+++ HackwichThree(redux)/ViewController.swift
@@ -8,16 +8,28 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var firstNameLabel: UILabel!
     
+    @IBOutlet var majorLabel: UILabel!
+    @IBOutlet var majorIs: UILabel!
+    @IBOutlet var classesLabel: UILabel!
     
+    @IBOutlet var aboutMeLabel: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     //set firstLabel to the string "About"
     
+        self.title = "About Me"
+    
         
-      self.firstLabel.text = "About"
+        self.firstLabel.text = "My Name is:"
+        self.firstNameLabel.text = "Evan"
+        self.majorLabel.text = "My Major is:"
+        self.majorIs.text = "Creative Media"
+        self.classesLabel.text = "My Spring 2018 Classes are:"
+        self.aboutMeLabel.text = "This class, an intro Photoshop class, a Literature class, Anime Class, and Asian Business class. I work for the YMCA as an Afterschool Leader, and my hobbies are playing video games (Rainbow Six Siege and Stardew Valley) and going out with my girlfriend."
 
     }
 
--- HackwichThree(redux)/ViewControllerTwo.swift
+++ HackwichThree(redux)/ViewControllerTwo.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
 
+    
+    @IBOutlet var secondTabLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.title = "Weather"
+        
     }
 
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
